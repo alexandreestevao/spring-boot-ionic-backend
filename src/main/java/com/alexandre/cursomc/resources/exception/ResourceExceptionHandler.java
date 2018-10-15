dced2964e78e289fd44aaf559992f28980538ebe
Alexandre <alexandresestevao@gmail.com>
2018-10-15 20:50:29 -0300

    Deletando uma Categoria com DELETE

--- src/main/java/com/alexandre/cursomc/resources/exception/ResourceExceptionHandler.java
+++ src/main/java/com/alexandre/cursomc/resources/exception/ResourceExceptionHandler.java
@@ -7,6 +7,7 @@ import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.ControllerAdvice;
 import org.springframework.web.bind.annotation.ExceptionHandler;
 
+import com.alexandre.cursomc.services.exceptions.ConstraintException;
 import com.alexandre.cursomc.services.exceptions.ObjectNotFoundException;
 
 @ControllerAdvice
@@ -17,6 +18,14 @@ public class ResourceExceptionHandler {
 		
 		StandardError err = new StandardError(HttpStatus.NOT_FOUND.value(), e.getMessage(), System.currentTimeMillis());
 		return ResponseEntity.status(HttpStatus.NOT_FOUND).body(err);
+		
+	}
+	
+	@ExceptionHandler(ConstraintException.class)
+	public ResponseEntity<StandardError> constraintException(ConstraintException e, HttpServletRequest request){
+		
+		StandardError err = new StandardError(HttpStatus.BAD_REQUEST.value(), e.getMessage(), System.currentTimeMillis());
+		return ResponseEntity.status(HttpStatus.BAD_REQUEST).body(err);
 		
 	}
 
--- src/main/java/com/alexandre/cursomc/services/CategoriaService.java
+++ src/main/java/com/alexandre/cursomc/services/CategoriaService.java
@@ -2,11 +2,13 @@ package com.alexandre.cursomc.services;
 
 import java.util.Optional;
 
+import org.hibernate.exception.ConstraintViolationException;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
 import com.alexandre.cursomc.domain.Categoria;
 import com.alexandre.cursomc.repositories.CategoriaRepository;
+import com.alexandre.cursomc.services.exceptions.ConstraintException;
 import com.alexandre.cursomc.services.exceptions.ObjectNotFoundException;
 
 @Service
@@ -29,6 +31,16 @@ public class CategoriaService {
 	public Categoria update(Categoria categoria) {
 		find(categoria.getId());
 		return repo.save(categoria);
+	}
+
+	public void delete(Integer id) {
+		find(id);
+		try {
+			repo.deleteById(id);
+		}
+		catch (ConstraintViolationException e) {
+			throw new ConstraintException("Não é possível excluir uma categoria associada a um produto!");
+		}
 	}
 
 }
